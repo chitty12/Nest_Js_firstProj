42d25e88125114f5b79a47a8b581ea9a40d8cc9f
chitty12 <cty12angel@naver.com>
2023-11-23 22:44:04 +0900

    NestJs : service 생성

--- board-app/src/boards/boards.module.ts
+++ board-app/src/boards/boards.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
 // nest(using nest cli) g(generate) module boards(name)
 
 @Module({
   controllers: [BoardsController],
+  providers: [BoardsService],
 })
 export class BoardsModule {}
 
---
+++ board-app/src/boards/boards.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+// service 란?
+// 소프트웨어 개발내의 공통 개념.
+// @Injectable 데코레이터로 감싸져서 모듈에 제공되며, 서비스 인스턴스는 애플리케이션 전체에서 사용가능.
+// controller 에서 데이터의 유효성 체크를 하거나 DB에 아이템을 생성하는 등의 작업을 처리.
+
+// nest g service boards --no-spec
+
+@Injectable()
+export class BoardsService {}
