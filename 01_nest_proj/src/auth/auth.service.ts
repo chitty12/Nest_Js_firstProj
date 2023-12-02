8cb356fb3936186d28fd897f60734b5a2b0b425d
chitty12 <cty12angel@naver.com>
2023-12-02 20:16:32 +0900

    bcrypt 추가

--- 01_nest_proj/src/auth/auth.service.ts
+++ 01_nest_proj/src/auth/auth.service.ts
@@ -16,7 +16,16 @@ export class AuthService {
   ) {}
 
   async signUp(AuthCredentialsDto: AuthCredentialsDto) {
-    this.AuthRepository.create(AuthCredentialsDto);
+    const { userName, password } = AuthCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPw = await bcrypt.hash(password, salt);
+
+    await this.AuthRepository.create({
+      userName,
+      password: hashedPw,
+    });
+
     return;
   }
 
@@ -36,12 +45,6 @@ export class AuthService {
     } else throw new UnauthorizedException('login failed');
   }
 }
-// salt = await bcrypt.genSalt();
-// hashedPw = await bcrypt.hash(password, salt);
-
-// user = this.creat({ username, password: hashedPw });
-
-//
 
 // jwt(JSON Web Token) : 당사자간 정보를 JSON 객체로 안전하게 전송하기위한 컴팩트하고 독립적인 방식을 정의하는 개방형 표준
 // 정보전달시 유저 권한 등을 체크하기 위해 사용되는 모듈
@@ -49,3 +52,9 @@ export class AuthService {
 // Payload(유저 정보, 만료 기간, 주제 등등)
 // Verify Signature(토큰이 보낸 사람에 의해 서명되어, 변경되지 않았는지 확인하는데 사용되는 서명<사용자가 설정한 Secret Key 포함>)
 // client 요청의 headers, payload 와 서버의 secret key 를 이용해 signature 생성.
+
+// 해시 함수 : 임의의 길이의 데이터를 고정된 길이의 데이터로 매핑하는 함수
+// 원래의 데이터(key) => 해시 함수를 수행한 결과값(hash value)  :  hashing 하다
+// hash collision : 서로 다른 두 개의 키가 같은 해시값을 가지는 경우.
+// 항상 1:1인 경우로 만드는 것 자체도 거의 불가능하지만, 만들더라도 메모리 차지가 큼 => 해시 충돌을 최소화하고 충돌에 대비한 대응 방안을 만드는 것이 더 중요!
+// 해시값으로 키 유추불가, 같은 키에 대해 항상 같은 해시값을 반환.
